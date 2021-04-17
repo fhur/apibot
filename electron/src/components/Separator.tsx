221fac9695a0498311b453da5043cf3480e3dcdd
Fernando Hurtado <fernandohur@gmail.com>
2021-04-17 22:27:50 +0200

    dropped electron/

--- electron/src/components/Separator.tsx
+++
@@ -1,8 +0,0 @@
-import { Colors } from "@blueprintjs/core";
-import styled from "@emotion/styled";
-
-export const Separator = styled.div`
-  width: 6px;
-  height: 10px;
-  background-color: ${Colors.LIGHT_GRAY1};
-`;
